b8f0f435be141499501ba9cb90e3c5ee286c7ca1
LACRIKA1 <tchaikovskyslava@yandex.ru>
2025-02-14 22:29:52 +0300

    first commit

---
+++ Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> chisla = new List<int>();
+            for (int i = 0; i < 10; i++) {
+
+                chisla.Add(i);
+
+                Console.WriteLine(chisla[i]);
+            }
+       
+        }
+    }
+}
